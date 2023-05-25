95303e489de7877a7ec6e7df3069b0f499e84f35
Nguyễn Ngọc Hiếu <76154814+NguyenNgocHieuu@users.noreply.github.com>
2023-05-25 21:24:59 +0700

    update trang home

--- thematic-java-project-2023/src/main/java/net/javaguides/springboot/controller/ProductController.java
+++ thematic-java-project-2023/src/main/java/net/javaguides/springboot/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
